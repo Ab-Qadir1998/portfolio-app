8919810a7c310344e52934390058dbec7c35d2a2
Abdul Qadir <abdulqadir12356@gmail.com>
2022-03-20 22:03:39 +0500

    set the modal functionality on image click

--- src/components/PortfolioContent/PortfolioContent.jsx
+++ src/components/PortfolioContent/PortfolioContent.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PortfolioContent.css";
 
 const PortfolioContent = ({ images }) => {
+  const [ openModal, setopenModal ] = useState(false)
+  const [ modalImage, setModalImage ] = useState('')
+  const getImage = (imageSrc)=> {
+    setModalImage(imageSrc)
+    setopenModal(true)
+  }
   return (
+    <>
+    {openModal ? (
+      <div className={openModal ? 'modal opened' : 'modal'}>
+      <div className="modalBody">
+      <button className="close_btn" onClick={()=> setopenModal(false)}>&times;</button>
+      <img src={modalImage}/>
+      </div>
+    </div>
+    ) : null}
     <div className="portfolio__content-wrapper">
       <div className="portfolio__box">
         <ul className="portfolio__item">
-          {images.map((image, index) => (
-            <li key={index} >
-              {<img src={image}  alt="portfolio_images" />}
+          {images.map((item, index) => (
+            <li key={index} onClick={()=> getImage(item.imageSrc)}>
+              {<img src={item.imageSrc}  alt="portfolio_images"/>}
             </li>
           ))}
         </ul>
       </div>
     </div>
+    </>
   );
 };
+
 
 export default PortfolioContent;
--- src/constants/images.js
+++ src/constants/images.js
@@ -11,6 +11,23 @@ import restaurant from "../assets/images/restaurant.png";
 import netflix from "../assets/images/netflix.png";
 import air from "../assets/images/airbnb-clone.png";
 
-export const allImages = [ja, blu, victoria, opulent, flatstone, ewa];
-export const reactImages = [shoe, restaurant, netflix, air, todo, tracker];
+// export const allImages = [ja, blu, victoria, opulent, flatstone, ewa];
+export const allImages = [
+    {id : 1, imageSrc : ja},
+    {id : 2, imageSrc : blu},
+    {id : 3, imageSrc : victoria},
+    {id : 4, imageSrc : opulent},
+    {id : 5, imageSrc : flatstone},
+    {id : 6, imageSrc : ewa},
+
+]
+// export const reactImages = [shoe, restaurant, netflix, air, todo, tracker];
+export const reactImages = [
+    {id : 1, imageSrc : tracker},
+    {id : 2, imageSrc : shoe},
+    {id : 3, imageSrc : restaurant},
+    {id : 4, imageSrc : netflix},
+    {id : 5, imageSrc : air},
+    {id : 6, imageSrc : todo},
+]
 // export default allImages;
